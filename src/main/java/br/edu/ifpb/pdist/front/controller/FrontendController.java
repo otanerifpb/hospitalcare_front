55939a6798ffacdf10ad1bc183703296f522e006
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-10-21 19:11:29 -0300

    Adicionar no GitHub

--- src/main/java/br/edu/ifpb/pdist/front/controller/FrontendController.java
+++ src/main/java/br/edu/ifpb/pdist/front/controller/FrontendController.java
@@ -1,8 +1,6 @@
 package br.edu.ifpb.pdist.front.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
