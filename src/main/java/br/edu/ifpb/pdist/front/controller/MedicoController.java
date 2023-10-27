d3f41704e9642265c17c7526438777b2a1b4a3d6
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-10-27 03:16:11 -0300

    Atualização Controller Médico

--- src/main/java/br/edu/ifpb/pdist/front/controller/MedicoController.java
+++ src/main/java/br/edu/ifpb/pdist/front/controller/MedicoController.java
@@ -28,9 +28,11 @@ public class MedicoController {
     @Autowired
     private RestTemplate restTemplate;
 
-    // Injeção do localhoste do application.properties
-    @Value("${backend.8081}")
-    private String localhost1;
+    // Injeção do localhoste do application.properties para Médico
+    //@Value("${backend.url}")
+    //private String localhost1;
+
+    private String localhost = "http://localhost:8081/medico";
 
     // Ativa o menu Médico na barra de navegação
     @ModelAttribute("menu")
@@ -41,9 +43,9 @@ public class MedicoController {
     // Rota para acessar a lista pelo menu
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView listAll(ModelAndView mav) {
-        String url = localhost1 + "/medico";
-        // List<Medico> opMedicos = restTemplate.getForObject("http://localhost:8081/medico", List.class); 
-        List<Medico> opMedicos = restTemplate.getForObject(url, List.class);      
+        //String url = localhost + "/medico";
+        //List<Medico> opMedicos = restTemplate.getForObject("http://localhost:8081/medico", List.class); 
+        List<Medico> opMedicos = restTemplate.getForObject(localhost, List.class);      
         mav.addObject("medico", opMedicos);
         mav.setViewName("medico/listMedico");
         return mav;
@@ -67,7 +69,7 @@ public class MedicoController {
     // Rota para cadastrar um Médico no Sitema
     @RequestMapping(method = RequestMethod.POST)
     public ModelAndView save(@ModelAttribute Medico medico , ModelAndView mav, RedirectAttributes redAttrs) {
-        String url = localhost1 + "/medico/save";
+        String url = localhost + "/save";
         //ResponseEntity<Medico> response = restTemplate.postForEntity("http://localhost:8081/medico/save", medico, Medico.class);
         ResponseEntity<Medico> response = restTemplate.postForEntity(url, medico, Medico.class);
         if (response.getStatusCode() == HttpStatus.CREATED) {
@@ -86,7 +88,8 @@ public class MedicoController {
     // Rota para preencer os dados do formunlário de atualização com dados do banco 
     @RequestMapping("/{id}")
     public ModelAndView getMedicoById(@PathVariable(value = "id") Integer id, ModelAndView mav) {
-        String url = localhost1 + "/medico/" + id; // Substitua 1 pelo ID desejado.
+        //String url = "http://localhost:8081/medico/" + id; 
+        String url = localhost + "/" + id; 
         ResponseEntity<Medico> response = restTemplate.getForEntity(url, Medico.class);
         if (response.getStatusCode() == HttpStatus.OK) {
             Medico medico =  response.getBody();
@@ -102,7 +105,7 @@ public class MedicoController {
     // // Rota para atualizar um Médico na lista pelo formUpMedico
     @RequestMapping(value="/update", method = RequestMethod.POST)
     public ModelAndView updade(Medico medico, ModelAndView mav, RedirectAttributes redAttrs) {
-        String url = localhost1 + "/medico/update";
+        String url = localhost + "/update";
         //ResponseEntity<Medico> response = restTemplate.postForEntity("http://localhost:8081/medico/update", medico, Medico.class);
         ResponseEntity<Medico> response = restTemplate.postForEntity(url, medico, Medico.class);
         if (response.getStatusCode() == HttpStatus.OK) {
@@ -122,7 +125,7 @@ public class MedicoController {
    @RequestMapping("{id}/delete")
     public ModelAndView deleteById(@PathVariable(value = "id") Integer id, ModelAndView mav, RedirectAttributes redAttrs) {
         try {
-            String url = localhost1 + "/medico/delete/" + id;
+            String url = localhost + "/delete/" + id;
             restTemplate.delete(url);
             redAttrs.addFlashAttribute("succesMensagem", "Medico id: " +id+ " deletado com sucesso!!");
         } catch (HttpClientErrorException e) {
