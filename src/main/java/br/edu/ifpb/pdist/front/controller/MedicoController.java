b3c1847daee3d39c22dff388a361ebac7ebf4397
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-11-27 19:44:21 -0300

    url gateway medico

--- src/main/java/br/edu/ifpb/pdist/front/controller/MedicoController.java
+++ src/main/java/br/edu/ifpb/pdist/front/controller/MedicoController.java
@@ -32,7 +32,7 @@ public class MedicoController {
     //private String localhost;
 
     // private String localhost = "http://localhost:8081/medico";
-    private String localhost = "https://apigatewayconfig-18d04b6103e0.herokuapp.com/medico";
+    private String localhost = "https://gatewayhospital-0433a88d53ad.herokuapp.com/medico";
     
 
 
